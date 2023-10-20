29737c8b0acf808a41fb96a128c2f9ff2b1d38ed
irfan2543 <muhammadirfansyah66@gmail.com>
2023-10-20 23:29:14 +0700

    Penambahan Fitur Dark dan Link Button Daftar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
                 <img class="w-3/4 h-3/4 ml-16" src="{{asset('assets/img/MyCuan_Image.png')}}" alt="Desain MyCuan">
             </div>
         </div>
-        <div class="w-1/2 bg-[#13A89E] max-h-full">
+        <div class="w-1/2 bg-[#13A89E] max-h-full ">
             <div class="flex flex-col w-3/4 h-3/4 mx-auto mt-2 content-center">
                 <div class="w-8/12 mx-auto">
                     <h1 class="text-white mt-28 font-bold font-poppins text-3xl">Selamat Datang</h1>
@@ -28,7 +28,7 @@
                         <div
                             class="flex flex-row items-center w-full bg-white rounded-md hover:border-indigo-500 border-transparent border-2 mt-2">
                             <x-input name="email" type="email" id="email"
-                                class="w-full py-2.5 ml-3 px-0 border-transparent focus:border-transparent focus:ring-0 dark:bg-white dark:border-transparent dark:focus:ring-0 dark:focus:border-transparent dark:text-black"
+                                class="w-full py-2.5 ml-3 px-0 border-transparent focus:border-transparent focus:ring-0 dark:bg-white dark:border-transparent dark:focus:ring-0 dark:focus:border-transparent"
                                 placeholder="{{ __('Silakan masukan alamat email Anda') }}" required
                                 autocomplete="username" :value="old('email')" />
                             <x-input-error :messages="$errors->get('email')" class="mt-2" />
@@ -39,7 +39,7 @@
                             value="{{ __('Kata Sandi')}}" />
                         <div class="flex flex-row items-center w-full bg-white rounded-md hover:border-indigo-500 border-transparent border-2 mt-2">
                             <x-input name="password" type="password" id="password"
-                                class="w-full py-2.5 ml-3 px-0 border-transparent focus:border-transparent focus:ring-0 dark:bg-white dark:border-transparent dark:focus:ring-0 dark:focus:border-transparent dark:text-black"
+                                class="w-full py-2.5 ml-3 px-0 border-transparent focus:border-transparent focus:ring-0 dark:bg-white dark:border-transparent dark:focus:ring-0 dark:focus:border-transparent"
                                 placeholder="{{ __('Silakan masukan kata sandi Anda') }}" required
                                 autocomplete="current-password" />
                                 <img class="w-6 h-6 mr-2" src="{{asset('assets/img/eye-hidden.png')}}" alt="Hidden Password" id="eye-hidden" onclick="show()">
@@ -50,13 +50,13 @@
                     <div class="flex flex-wrap w-8/12 mt-6 mx-auto">
                         <div class="w-1/2 content-center">
                             <x-label class="block" for="remember">
-                                <span class="text-white text-xl font-poppins">Ingat Saya</span>
+                                <span class="text-white text-xl font-poppins">{{ __('Ingat Saya') }}</span>
                                 <x-input class="rounded-md mb-1 dark:bg-white dark:border-transparent dark:focus:ring-0 dark:focus:border-transparent dark:text-blue-600" type="checkbox" id="remember" name="remember" />
                             </x-label>
                         </div>
                         <div class="w-1/2">
                             @if (Route::has('password.request'))
-                            <a href="{{ route('password.request') }}" class="float-right text-white text-xl font-poppins">Lupa Kata Sandi?
+                            <a href="{{ route('password.request') }}" class="float-right text-white text-xl font-poppins">{{ __('Lupa Kata Sandi?') }}
                             </a>
                             @endif
                         </div>
@@ -65,11 +65,14 @@
                         <button class="w-full mt-3 h-10 bg-[#186F65] text-white font-bold text-base font-worksans justify-center rounded-md hover:bg-white hover:text-[#186F65]">
                             {{ __('MASUK') }}
                         </button>
-                        <button class="w-full mt-3 h-10 bg-white text-[#186F65] font-bold text-base font-worksans justify-center rounded-md hover:bg-[#186F65] hover:text-white">
-                            {{ __('DAFTAR') }}
-                        </button>
                     </div>
                 </form>
+                <div class="flex flex-col w-8/12 mt-6 mx-auto justify-center">
+                    
+                <button class="w-full h-10 bg-white text-[#186F65] font-bold text-base font-worksans justify-center rounded-md hover:bg-[#186F65] hover:text-white" onclick="window.location='{{ route('register') }}'">
+                    {{ __('DAFTAR') }}
+                </button>
+                </div>
             </div>
         </div>
     </div>
