a68d67c61ae6172aec9e69dc9547c97870684f5d
ufairahbashir <ufairahbashir@gmail.com>
2023-10-21 04:42:22 +0700

    page lupa password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,46 @@
 <x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+    <!-- Session Status -->
+    @if (session('status'))
+    <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+        {{ session('status') }}
+    </div>
+    @endif
 
-        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+
+    <div class="flex flex-wrap max-h-full">
+        <div class="w-1/2 h-screen">
+            <div class="flex flex-wrap items-center h-40 ">
+                <img src="{{asset('assets/img/Logo_MyCuan.png')}}" alt="Logo MyCuan" class="w-20 h-20 ml-28">
+                <p class="text-[#868686] ml-8 mt-4 font-semibold font-poppins text-2xl">MyCuan</p>
+            </div>
+            <div>
+                <img class="w-3/4 h-3/4 ml-16" src="{{asset('assets/img/MyCuan_Image.png')}}" alt="Desain MyCuan">
+            </div>
         </div>
+        <div class="w-1/2 bg-[#13A89E] max-h-full ">
+            <div class="flex flex-col mx-auto mt-2">
+                <div class="flex items-center justify-center mx-auto">
+                    <h1 class="text-white mt-28 font-bold font-poppins text-3xl items-center justify-center">Lupa Kata Sandi</h1>
+                </div>
+                <div class="flex mb-4 text-sm text-white items-center justify-center mt-4">
+                    {{ __('Masukan alamat email yang telah terdaftar untuk menerima email reset kata sandi') }}
+                </div>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-                {{ session('status') }}
-            </div>
-        @endif
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
 
-        <x-validation-errors class="mb-4" />
+                    <div class="w-8/12 mt-16 mx-auto">
+                        <x-input-label class="text-white text-xl font-poppins dark:text-white" for="email" value="{{ __('Email')}}" />
+                        <div class="flex flex-row items-center w-full bg-white rounded-md hover:border-indigo-500 border-transparent border-2 mt-2">
+                            <x-input name="email" type="email" id="email" class="w-full py-2.5 ml-3 px-0 border-transparent focus:border-transparent focus:ring-0 dark:bg-white dark:border-transparent dark:focus:ring-0 dark:focus:border-transparent" placeholder="{{ __('Silakan masukan alamat email Anda') }}" required autocomplete="username" :value="old('email')" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        </div>
+                    </div>
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
+                    <div class="flex flex-col w-8/12 mt-6 mx-auto justify-center">
+                        <x-button class="justify-center">
+                            {{ __('Kirim') }}
+                        </x-button>
+                    </div>
+                </form>
 </x-guest-layout>
