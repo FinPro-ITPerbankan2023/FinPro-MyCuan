eff85f2477546eef8bcc0f90558b384fe0ff503a
Ikrar Bagaskara <ikrarb95@gmail.com>
2023-10-31 08:42:01 +0700

    INFO: Disable darkmode support

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->id('admin')
             ->path('admin')
+            ->darkMode(false)
             ->login()
             ->breadcrumbs(false)
             ->plugin(FilamentProgressbarPlugin::make()->color('#29b'))
